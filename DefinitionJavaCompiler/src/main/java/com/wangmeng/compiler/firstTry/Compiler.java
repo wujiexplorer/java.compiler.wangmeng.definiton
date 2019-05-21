a6b5f5c9eecf435f67d11089bc6fde722ba0d056
wangmeng <wangmeng@hztianque.com>
2019-05-21 23:19:25 +0800

    EOI current.isEmpty()，return EOI 词法解析 入门级别

---
+++ DefinitionJavaCompiler/src/main/java/com/wangmeng/compiler/firstTry/Compiler.java
@@ -0,0 +1,11 @@
+package com.wangmeng.compiler.firstTry;
+
+public class Compiler {
+
+	public static void main(String[] args) {
+		Lexer lexer = new Lexer();
+		lexer.runLexer();;
+//		BasicParser basic_parser = new BasicParser(lexer);
+//		basic_parser.statements();
+	}
+}
